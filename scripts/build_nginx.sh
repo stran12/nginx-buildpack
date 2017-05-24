7ad2c52d181705c58ee9316291aa5f805e4086bb
Stephen Tran <Stephen.Tran@us.redbullmediahouse.com>
2017-05-24 16:46:36 -0700

    first try

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -12,12 +12,10 @@
 NGINX_VERSION=${NGINX_VERSION-1.12.0}
 PCRE_VERSION=${PCRE_VERSION-8.21}
 HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
-RESTY_GEOIP2_VERSION=2.0
-geoip_db=http://s3.amazonaws.com/rbtv-v3/geoip_db-latest.tgz
 
-nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
-headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
+nginx_tarball_url=https://s3.amazonaws.com/rbtv-v3/nginx-${NGINX_VERSION}.tar.gz
+pcre_tarball_url=http://s3.amazonaws.com/rbtv-v3/pcre-$PCRE_VERSION}.tar.gz
+headers_more_nginx_module_url=https://s3.amazonaws.com/rbtv-v3/headers-more-nginx-module-${HEADERS_MORE_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
@@ -28,73 +26,23 @@ python -m SimpleHTTPServer $PORT &
 cd $temp_dir
 echo "Temp dir: $temp_dir"
 
-# echo "Downloading $nginx_tarball_url"
-# curl -L $nginx_tarball_url | tar xzv
+echo "Downloading $nginx_tarball_url"
+curl -L $nginx_tarball_url | tar xzv
 
-# echo "Downloading $pcre_tarball_url"
-# (cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
+echo "Downloading $pcre_tarball_url"
+(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
 
-# echo "Downloading $headers_more_nginx_module_url"
-# (cd nginx-${NGINX_VERSION} && curl -L $headers_more_nginx_module_url | tar xvz )
+echo "Downloading $headers_more_nginx_module_url"
+(cd nginx-${NGINX_VERSION} && curl -L $headers_more_nginx_module_url | tar xvz )
 
-# Start of the extra stuff
-echo "==============================================="
-apt-get -y update && apt-get -y install wget software-properties-common python-software-properties
-
-apt-get clean 
-cd /var/lib/apt 
-mv lists lists.old 
-mkdir -p lists/partial 
-apt-get clean 
-apt-get update
-
-add-apt-repository ppa:maxmind/ppa
-
-apt-get -y install zip geoip-database libgeoip1 libgeoip-dev libmaxminddb0 libmaxminddb-dev mmdb-bin nginx build-essential libpcre3-dev libssl-dev luarocks
-
-# cd /tmp && wget -c $geoip_db && tar zxvf $geoip_db
-cd /tmp && wget -c http://codeload.github.com/leev/ngx_http_geoip2_module/tar.gz/${RESTY_GEOIP2_VERSION} && \
-    tar zxvf ${RESTY_GEOIP2_VERSION}
-
-wget -c http://openresty.org/download/openresty-1.11.2.1.tar.gz
-tar zxvf openresty-1.11.2.1.tar.gz
 (
-	cd openresty-1.11.2.1
+	cd nginx-${NGINX_VERSION}
 	./configure \
-		--add-module=/tmp/ngx_http_geoip2_module-${RESTY_GEOIP2_VERSION} \
-		--sbin-path=/usr/sbin/nginx \
-		--conf-path=/etc/nginx/nginx.conf \
-		--error-log-path=/var/log/nginx/error.log \
-		--http-client-body-temp-path=/var/lib/nginx/body \
-		--http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
-		--http-log-path=/var/log/nginx/access.log \
-		--http-proxy-temp-path=/var/lib/nginx/proxy \ 
-		# --http-scgi-temp-path=/var/lib/nginx/scgi \ 
-		# --http-uwsgi-temp-path=/var/lib/nginx/uwsgi \
-		# --lock-path=/var/lock/nginx.lock \
-		# --pid-path=/var/run/nginx.pid \
-		--with-http_geoip_module \
-		--with-http_realip_module \
-		--with-luajit && \
-	luarocks install lua-resty-http && \
-	luarocks install lua-cjson && \
-	luarocks install lua-resty-jwt && \
-	luarocks install uuid && \
-	make && \
-	make install && \
-	apt-get -y autoclean && \
-	apt-get -y autoremove
+		--with-pcre=pcre-${PCRE_VERSION} \
+		--prefix=/tmp/nginx \
+		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+	make install
 )
-echo "==============================================="
-
-# (
-# 	cd nginx-${NGINX_VERSION}
-# 	./configure \
-# 		--with-pcre=pcre-${PCRE_VERSION} \
-# 		--prefix=/tmp/nginx \
-# 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
-# 	make install
-# )
 
 while true
 do
