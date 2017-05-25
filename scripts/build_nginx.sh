b8e60abb3455fb0998802abfdea4d80f7e66d1da
Stephen Tran <Stephen.Tran@us.redbullmediahouse.com>
2017-05-24 18:35:53 -0700

    make first

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -45,6 +45,7 @@ echo "Downloading $headers_more_nginx_module_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+	make	
 	make install
 )
 
