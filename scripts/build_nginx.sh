81107eddb683b1db5f15a467fa918b0ddc025b1b
Stephen Tran <Stephen.Tran@us.redbullmediahouse.com>
2017-07-07 14:16:04 -0700

    added some debug lines

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -57,7 +57,9 @@ echo "Downloading $headers_more_nginx_module_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+	echo "installing"	
 	make install
+	echo "finished installing"
 )
 
 while true
